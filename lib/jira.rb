7ac4e7c7d404a24bf2fa92b34de9aae769309b8c
Dominik Elsbroek <dominik.elsbroek@gmail.com>
2013-01-23 18:53:22 +0100

    added fixVersion as search option in lib/jira.rb and bin/JiRC

--- lib/jira.rb
+++ lib/jira.rb
@@ -29,28 +29,60 @@ module Jira
     API_PREFIX = "/rest/api/2"
     PROJECT_PREFIX = "project"
 
-
+    #
+    # opts must contains :user, :pass, and :host
+    # 
+    def self.projects(opts = {})
+      Jira::check_opts(opts)
+      uri = "#{opts[:host]}#{API_PREFIX}/#{PROJECT_PREFIX}"
+      execute_and_log_request_request(opts, uri)
+    end
+    
+    #
+    # opts must contains :project_key besides :user, :pass, and :host
+    # 
     def self.project_data(opts = {})
+      Jira::check_opts(opts)
       uri = "#{opts[:host]}#{API_PREFIX}/#{PROJECT_PREFIX}/#{opts[:project_key]}"
       execute_and_log_request_request(opts, uri)
     end
 
+    #
+    # opts must contains :project_key besides :user, :pass, and :host
+    # 
     def self.project_versions(opts = {})
+      Jira::check_opts(opts)
       uri = "#{opts[:host]}#{API_PREFIX}/#{PROJECT_PREFIX}/#{opts[:project_key]}/versions"
       uri << "?expand" if opts[:expand]
       execute_and_log_request_request(opts, uri)
     end
 
+    #
+    # opts must contains :version_id besides :user, :pass, and :host
+    # 
+    def self.version_data(opts = {})
+      Jira::check_opts(opts)
+      uri = "#{opts[:host]}#{API_PREFIX}/version/#{opts[:version_id]}"
+      uri << "?expand" if opts[:expand]
+      execute_and_log_request_request(opts, uri)
+    end
+
+
+
+    #
+    # opts must contains :version besides :user, :pass, and :host
+    # 
     def self.version(opts = {})
+      Jira::check_opts(opts)
       uri = "#{opts[:host]}#{API_PREFIX}#{VERSION_PREFIX}/#{opts[:version]}"
       execute_and_log_request_request(opts, uri)
     end
 
     private 
-    def self.execute_and_log_request_request(opts, req_path)
-      opts[:logger].debug "requesting: #{req_path}" unless opts[:logger].nil?
-      puts "requesting: #{req_path}" if opts[:verbose]
 
+    def self.execute_and_log_request_request(opts, req_path)
+      opts[:logger].debug "JIRA requesting: #{req_path}" unless opts[:logger].nil?
+      puts "requesting: #{req_path}" if opts[:verbose]
 
       # FIXME: if the user or password contains any characters that could break 
       # authentification, such as a colon, they will break authentification here!
@@ -62,20 +94,31 @@ module Jira
   class Search
 
     SEARCH_PATH = '/rest/api/latest/search.json'
-    KEYS = %w|project issuetype affectedVersion assignee|
+    KEYS = %w|project issuetype affectedVersion fixVersion assignee|
     # if we don't have symbols here options can't be checked for search parameters
-    OPTIONS = %w|startAt maxResults expand|.map{|elem| elem.to_sym}
+    OPTIONS = %w|startAt maxResults expand|.map {|elem| elem.to_sym}
 
     def self.search(opts = {})
       # The only parameter that needs escaping (future version should use an array)
-      unless opts[:affectedVersion].nil?
-        opts[:affectedVersion] = "%22#{opts[:affectedVersion]}%22"
+      %w|affectedVersion fixVersion|.each do |esc|
+        opts[esc.to_sym] = "%22#{URI.encode(opts[esc.to_sym])}%22" unless opts[esc.to_sym].nil?
       end
       query = ""
       KEYS.each do |var|
         if opts[var.to_sym]
+          unless opts[var.to_sym].is_a?(Array)
+            query << "%20AND%20" if(0 < query.length)
+            query << "#{var}=#{opts[var.to_sym]}"
+            next
+          end
           query << "%20AND%20" if(0 < query.length)
-          query << "#{var}=#{opts[var.to_sym]}"
+          query << "("
+          started_at_idx = query.length
+          opts[var.to_sym].each do |elem|
+            query << "%20OR%20" if(started_at_idx < query.length)
+            query << "#{var}=#{elem}"
+          end
+          query << ")"
         end
       end
       unless (opts.keys & OPTIONS).empty?
@@ -89,13 +132,21 @@ module Jira
         end
       end
       opts[:logger].debug "requesting: #{opts[:host]}#{SEARCH_PATH}?jql=#{query}" unless opts[:logger].nil?
-      "requesting: #{opts[:host]}#{SEARCH_PATH}?jql=#{query}" if opts[:logger].nil? && opts[:verbose]
+      puts "requesting: #{opts[:host]}#{SEARCH_PATH}?jql=#{query}" if opts[:logger].nil? && opts[:verbose]
 
       # FIXME: if the user or password contains any characters that could break 
       # authentification, such as a colon, they will break authentification here!
       RestClient::Resource.new("#{opts[:host]}#{SEARCH_PATH}?jql=#{query}", opts[:user], opts[:pass]).get(:content_type => 'application/json')
     end
   end
+
+    
+  def self.check_opts(opts)
+    raise "user must not be empty" if opts[:user].nil? || opts[:user].blank? 
+    raise "password must not be empty" if opts[:pass].nil? || opts[:pass].blank?
+    raise "host must not be emtpy" if opts[:host].nil? || opts[:host].blank?
+  end
+
 end
 
 
